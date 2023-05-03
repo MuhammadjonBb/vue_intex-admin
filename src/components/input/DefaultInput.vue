f1b630ee52c8fdecb1ec4b9949be9681737f2da6
Muhammad Boborakhimov <muhammadzonboborahimov7@gmail.com>
2023-05-03 16:25:34 +0500

    modified defaultinput

--- src/components/input/DefaultInput.vue
+++ src/components/input/DefaultInput.vue
@@ -1,9 +1,8 @@
 <template>
   <label :for="name" class="font-weight-medium q-mb-md full-width" style="display: block;">
     {{ label }}
-    <q-input :rules="[val => !!val || 'Это обязательное поле!']" :id="name" borderless
-      v-model="inputStore.input[inputData.component][inputData.inputName]" :placeholder="placeholder"
-      class="q-mt-sm i-border q-px-md" :type="type" style="position: relative;">
+    <q-input :rules="rules" :id="name" borderless v-model="inputStore.input[inputData.component][inputData.inputName]"
+      :placeholder="placeholder" class="q-mt-sm i-border q-px-md" :type="type" style="position: relative;">
       <template v-if="icon" #append>
         <q-icon :name="icon" color="primary" />
       </template>
@@ -15,5 +14,5 @@
 import { useInputStore } from 'stores/moduls/input';
 
 const inputStore = useInputStore()
-const props = defineProps(['inputData', 'label', 'placeholder', 'type', 'name', 'icon'])
+const props = defineProps(['inputData', 'label', 'placeholder', 'type', 'name', 'icon', 'rules'])
 </script>
